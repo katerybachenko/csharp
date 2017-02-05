658dabfb792a6e9059323eaaed7a6a24a0ecf1da
Kate R <rybachenkok@gmail.com>
2017-02-05 22:13:25 +0200

    Adding two files as homework

---
+++ csharp/PrimitiveConvertor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class PrimitiveConvertor
+    {
+        int int1 = 122;
+        float float1 = 3.123456F;
+        char c = (char) 0;
+
+        public void floatToChar()
+        {
+            float1 = c;
+            Console.WriteLine("Input float value is 3.123456F Output char value is {0}", (char)122);
+        }
+        public void intToChar()
+        {
+            int1 = c;
+            Console.WriteLine("Input int value is 122 Output char value is {0}", (char)122);
+        }
+        public void charToInt()
+        {
+            c = (char) int1;
+            Console.WriteLine("Input char value is {0} Output int value is 122", (char)122);
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Program
+    {
+       static void Main(string[] args) 
+        {
+            WideningCasting widening = new WideningCasting();
+            widening.conversion();
+
+            NarrowingCasting narrowing = new NarrowingCasting();
+            narrowing.Narrowing();
+
+            PrimitiveConvertor primitive = new PrimitiveConvertor();
+            primitive.floatToChar();
+
+            PrimitiveConvertor primitive1 = new PrimitiveConvertor();
+            primitive1.intToChar();
+
+            PrimitiveConvertor primitive2 = new PrimitiveConvertor();
+            primitive2.charToInt();
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
